28a830ae777b2c4b0f7e123e9a32f46b1ed3628a
danieldecarval <daniel.decarvalhofernandes@he-arc.ch>
2015-03-21 09:31:33 +0100

    Ajout du projet + affichage iconNotification

---
+++ ScreenRecorder/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace ScreenRecorder
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            
+            //Minimize the window and don't put in the taskbar
+            /*WindowState = WindowState.Minimized;
+            ShowInTaskbar = false;*/
+
+            /*TaskbarIcon tbi = new TaskbarIcon();
+            tbi.Icon = Properties.Resources.Error;
+            tbi.ToolTipText = "hello world";*/
+            
+           
+            
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+        }
+
+       
+
+        
+    }
+}
